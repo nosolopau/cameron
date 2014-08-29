f717d84871e2028518fd7247c2ad457709819ce9
nosolopau <pau@nosolopau.com>
2014-08-29 18:29:45 +0200

    Added basic recipes system

--- app/models/server_factory.rb
+++ app/models/server_factory.rb
@@ -7,10 +7,11 @@ class ServerFactory
     barge.key.create(name: 'Cameron', public_key: `cat id_rsa.pub`) unless barge.key.show(fingerprint).success?
 
     # Create droplet
-    # new_droplet = barge.droplet.create(name: params[:name], size: '512mb', image: '5588928', region: params[:region], ssh_keys: [fingerprint]).droplet
-    new_droplet = nil
+    new_droplet = barge.droplet.create(name: params[:name], size: '512mb', image: '5588928', region: params[:region], ssh_keys: [fingerprint]).droplet
+    #new_droplet = nil
+
     new_server = DigitalOceanServer.new(params)
-    new_server.update_attributes(provider_id: new_droplet.id, memory: new_droplet.memory, cpus: new_droplet.vcpus, disk: new_droplet.vcpus, status: new_droplet.status) if new_droplet
+    new_server.update_attributes(provider_id: new_droplet.id, memory: new_droplet.memory, cpus: new_droplet.vcpus, disk: new_droplet.disk, status: new_droplet.status) if new_droplet
 
     new_server
   end
